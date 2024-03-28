a9d7b75b128a92a8951dca6df79831ec0fb93dd9
mfahadjbr <mfahadjbr@gmail.com>
2024-03-27 19:05:24 -0700

    new formula

---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const binformula = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'calculator',
+        message: 'Enter weight number = '
+    },
+    {
+        type: 'number',
+        name: 'calculator1',
+        message: 'Enter height number = '
+    }
+]);
+function BIMformula() {
+    let BIM = binformula.calculator / (binformula.calculator1 * binformula.calculator1);
+    console.log(BIM);
+}
+BIMformula();
